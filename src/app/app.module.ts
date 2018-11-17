fb5e9935311bcf21f5a5955b1cb2f6b19d9f7f3e
Ola Andersson <ola.bmw.andersson@gmail.com>
2018-11-18 00:15:22 +0100

    Add header and contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
